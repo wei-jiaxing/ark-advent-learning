c75fdf66c84bd8ecfbed340593ac1af74ab859d2
wei-jiaxing <wei-jiaxing@kayac.com>
2019-07-24 15:10:35 +0900

    コメント

--- Jobeet/lib/Jobeet/Controller/Job.pm
+++ Jobeet/lib/Jobeet/Controller/Job.pm
@@ -3,6 +3,8 @@ use Ark 'Controller';
 
 use Jobeet::Models;
 
+# index: Path('foo')なら /job/fooにマッチして、index.mtを使う
+# index: Localなら /job/indexにマッチして、index.mtを使う
 sub index: Path{
 	my($self, $c) = @_;
 	# stashはグローバル変数の入れ物、VとCの間で変数共有に使用される
